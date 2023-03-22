8ca682c534654674ab9553a444e2bc6afb878abe
1jack80 <work.davidmainoo@gmail.com>
2023-03-22 15:42:43 +0000

    generate synthetic data

--- datagen.py
+++ datagen.py
@@ -1,25 +1,22 @@
+
 from ctgan import CTGAN
 from ctgan import load_demo
+import pandas as pd
 
-real_data = load_demo()
+
+real_data = pd.read_csv("./heart_datagen.csv")
+
 
 # Names of the columns that are discrete
 discrete_columns = [
-    'workclass',
-    'education',
-    'marital-status',
-    'occupation',
-    'relationship',
-    'race',
-    'sex',
-    'native-country',
-    'income'
+"output",
 ]
 
-# ctgan = CTGAN(epochs=10)
-# ctgan.fit(real_data, discrete_columns)
+
+ctgan = CTGAN(epochs=10)
+ctgan.fit(real_data, discrete_columns)
 
 # Create synthetic data
-# synthetic_data = ctgan.sample(1000)
+synthetic_data = ctgan.sample(1197)
+print(synthetic_data)
 
-print(type(real_data))
